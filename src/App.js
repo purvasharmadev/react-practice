239d079e3ab7917bc1544875772c5a1051d9087d
Purva Sharma <71826879+purvasharmadev@users.noreply.github.com>
2021-01-01 21:43:55 +0530

    completed react-part-1 from NeogCamp lesson-5 guide

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React ,{useState} from "react";
 import "./styles.css";
+
+myDictionary = {
+  "1":"one",
+  "2":"two",
+  "3":"three",
+  "4":"four",
+  "5":"five",
+}
+
 
 export default function App() {
   var bg = "blue";
   var name = "jane";
   var welcome = "Welcome !!";
   var pf = "https://repl.it/@purvasharmadev";
+
+  // LikeHandler
+  var [like,setlike] = useState(0);
+  function likeHandler(){
+    console.log("cliked")
+    var newLike = like + 1;
+    setlike(newLike)
+  }
+// InputHandler
+  var [uName,setUname] = useState("");
+  function inputHandler(event){
+    // console.log("user is typing...")
+    var newUname = event.target.value;
+    var uName = myDictionary[newUname];
+    if(uName === undefined){
+      uName = "sorry :( no more numbers"
+    }
+    setUname(uName);
+  }
+
   return (
     <div className="App">
       <h1>Time To learn React</h1>
@@ -68,6 +97,27 @@ export default function App() {
       <h3 style={{ color: bg }}>
         {welcome} {name}
       </h3>
+
+      <h2>OnClick</h2>
+      <p>In react we dont need to declare eventListners </p>
+      <button onClick={likeHandler}>you liked me {like} times</button>
+
+      <h2>Render Cycle</h2>
+      <p> <strong>VISER</strong> <br/> <br/>
+      Render a <strong>V</strong>iew <br/>
+      User will <strong>I</strong>nteract <br/> Update the <strong>S</strong>tate in <strong>E</strong>vent Handler <br/>
+      After the update we <strong>R</strong>ender it again.
+      </p>
+
+      <h2>Handling data with input element</h2>
+      <p> we use <strong>OnChange event </strong> to get the value user is typing. <br/>
+      Update the value using <strong>useState</strong> <br/>
+      get the value in view using <strong>Event.target</strong> </p>
+
+
+      <h3>Enter a digit <small>e.g 1, 2, 3</small></h3>
+      <input onChange={inputHandler}/>
+      <h4>number:  {uName} </h4>
     </div>
   );
 }
